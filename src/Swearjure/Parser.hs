1530c5f56a7d05cb136f5d5559d03762e813e634
Jean Niklas L'orange <jeannikl@hypirion.com>
2015-02-04 23:24:12 +0100

    Add internal state to parser.

--- src/Swearjure/Parser.hs
+++ src/Swearjure/Parser.hs
@@ -16,6 +16,8 @@ import Data.Traversable (Traversable)
 import Swearjure.Errors
 import Text.ParserCombinators.Parsec
 
+type SwjParser = GenParser Char Bool
+
 data ParseVal p = PSym String -- qualified syms. Gur
                 | PString String
                 | PKw String
@@ -36,55 +38,55 @@ cljns :: String
 cljns = "clojure.core/"
 
 -- | Discards the result of the parser
-omit :: Parser p -> Parser ()
+omit :: SwjParser p -> SwjParser ()
 omit p = p >> return ()
 
 -- | Semi matches a semicolon and returns
-semi :: Parser Char
+semi :: SwjParser Char
 semi = char ';'
 
-commentLine :: Parser String
+commentLine :: SwjParser String
 commentLine = skipMany1 semi >> manyTill anyChar (omit newline <|> eof)
 
 -- | Filters out any kind of whitespace.
-whiteSpace :: Parser ()
+whiteSpace :: SwjParser ()
 whiteSpace = skipMany (omit (char ',') <|> omit space <|> omit commentLine
                        <|> try sharpWhites)
 
-sharpWhites :: Parser ()
+sharpWhites :: SwjParser ()
 sharpWhites = char '#' >> (sharp_ <|> sharpBang)
 
-sharp_ :: Parser ()
+sharp_ :: SwjParser ()
 sharp_ = lexeme (char '_') >> omit expr
 
-sharpBang :: Parser ()
+sharpBang :: SwjParser ()
 sharpBang = char '!' >> manyTill anyChar (omit newline <|> eof) >> return ()
 
-lexeme :: Parser p -> Parser p
+lexeme :: SwjParser p -> SwjParser p
 lexeme p = do x <- p
               whiteSpace
               return x
 
 -- fixme: These are.. nasty to do properly.
-startSymChar :: Parser Char
+startSymChar :: SwjParser Char
 startSymChar = oneOf "+-*/!=<>?_&$%"
 
-symChar :: Parser Char
+symChar :: SwjParser Char
 symChar = startSymChar <|> oneOf "#'"
 
-symString :: Parser String
+symString :: SwjParser String
 symString = do x <- startSymChar
                xs <- many symChar
                return $ x : xs
 
-symbol :: Parser PVal
+symbol :: SwjParser PVal
 symbol = (Fix . PSym)  <$> lexeme symString
 
 -- TODO: qualified kws
-keyword :: Parser PVal
+keyword :: SwjParser PVal
 keyword = Fix . PKw <$> lexeme (char ':' >> symString)
 
-malString :: Parser PVal
+malString :: SwjParser PVal
 malString = Fix . PString <$> (lexeme $ between (char '"') (char '"')
                                (many stringChar))
   where stringChar = stringLetter <|> stringEscape
@@ -93,40 +95,40 @@ malString = Fix . PString <$> (lexeme $ between (char '"') (char '"')
         -- Todo: Fix this properly?
         stringEscape = char '\\' >> oneOf "\\\""
 
-delimited :: Char -> Char -> Parser c -> Parser c
+delimited :: Char -> Char -> SwjParser c -> SwjParser c
 delimited start stop = between (lexeme $ char start) (lexeme $ char stop)
 
-list :: Parser PVal
+list :: SwjParser PVal
 list = Fix . PList <$> delimited '(' ')' (many expr)
 
-vec :: Parser PVal
+vec :: SwjParser PVal
 vec = Fix . PVec <$> delimited '[' ']' (many expr)
 
-hashMap :: Parser PVal
+hashMap :: SwjParser PVal
 hashMap = Fix . PHM <$> delimited '{' '}' (many pair)
   where pair = (,) <$> expr <*> expr
 
 call :: String -> [PVal] -> PVal
 call s es = Fix . PList $ (Fix . PSym) s : es
 
-sugared :: Char -> String -> Parser PVal
+sugared :: Char -> String -> SwjParser PVal
 sugared c s = do (omit . lexeme . char) c
                  e <- expr
                  return $ call (cljns ++ s) [e]
 
-quote :: Parser PVal
+quote :: SwjParser PVal
 quote = sugared '\'' "quote"
 
 -- Alright, this is obv. wrong. Need to look into the reader properly, because
 -- it does some fancy read time replacement.
-backquote :: Parser PVal
+backquote :: SwjParser PVal
 backquote = sugared '`' "quote"
 
-deref :: Parser PVal
+deref :: SwjParser PVal
 deref = sugared '@' "deref"
 
 -- this needs a bit of help to avoid turning ~@foo into (unquote (deref foo))
-unquote :: Parser PVal
+unquote :: SwjParser PVal
 unquote = do omit $ char '~'
              splice <- optionMaybe $ char '@'
              e <- expr
@@ -135,7 +137,7 @@ unquote = do omit $ char '~'
         unquoteType Nothing = cljns ++ "unquote"
 
 -- TODO: Should use attr to tag on vals.
---meta :: Parser PVal
+--meta :: SwjParser PVal
 --meta = do omit $ char '^'
 --          m <- expr
 --          e <- expr
@@ -143,26 +145,26 @@ unquote = do omit $ char '~'
 
 -- sharpies be here
 
-sharp :: Parser PVal
+sharp :: SwjParser PVal
 sharp = char '#' >> (sharpQuote <|> set <|> fnLit)
 
-sharpQuote :: Parser PVal
+sharpQuote :: SwjParser PVal
 sharpQuote = sugared '\'' "var"
 
-fnLit :: Parser PVal
+fnLit :: SwjParser PVal
 fnLit = Fix . PFnLit <$> delimited '(' ')' (many expr)
 
-set :: Parser PVal
+set :: SwjParser PVal
 set = Fix . PSet <$> delimited '{' '}' (many expr)
 
 -- are there more? More non-whitespacey, that is.
 
-expr :: Parser PVal
+expr :: SwjParser PVal
 expr = list <|> vec <|> symbol <|> keyword <|> malString <|> hashMap
        <|> quote <|> backquote <|> deref <|> unquote <|> sharp
 
 readAst :: String -> Either SwjError (Maybe PVal)
 readAst s = throwLeftMap SyntaxError $
-             parse (whiteSpace >> optionMaybe expr <* eof) "" s
+             runParser (whiteSpace >> optionMaybe expr <* eof) False "" s
   where throwLeftMap f (Left x) = throwError (f x)
         throwLeftMap _ (Right x) = return x
