2517afefceedb38007dd20731915d7fb58930b6f
Jean Niklas L'orange <jeannikl@hypirion.com>
2015-02-07 14:28:53 +0100

    Remember history of last 1000 commands.

--- src/Main.hs
+++ src/Main.hs
@@ -1,25 +1,35 @@
 {-# OPTIONS_GHC -Wall -Werror #-}
+{-# LANGUAGE OverloadedStrings #-}
 
-import Control.Monad.Except
-import Control.Monad.State
-import Swearjure.AST (prStr)
-import Swearjure.Errors
-import Swearjure.Reader
-import System.Console.Readline (readline, addHistory)
+import           Control.Applicative ((<$>))
+import           Control.Monad.Except
+import           Control.Monad.State
+import           Data.Foldable (toList)
+import           Data.Sequence
+import qualified Data.Sequence as S
+import           Filesystem hiding (readFile, writeFile)
+import           Filesystem.Path.CurrentOS
+import           Swearjure.AST (prStr)
+import           Swearjure.Errors
+import           Swearjure.Reader
+import           System.Console.Readline (readline, addHistory)
 
 main :: IO ()
-main = loop 1
+main = do hist <- readHistory
+          runStateT (loop 1) hist >> return ()
 
-loop :: Int -> IO ()
+loop :: Int -> StateT (Seq String) IO ()
 loop gsymCount
-  = do input <- readline "swj> "
+  = do input <- liftIO $ readline "swj> "
        case input of
-        Nothing -> return ()
+        Nothing -> do get >>= liftIO . writeHistory
+                      return ()
         Just str ->
-          do addHistory str
+          do liftIO $ addHistory str
+             modify (|> str)
              let (res, symCount) = (re gsymCount str)
              case res of
-              Just x -> putStrLn x
+              Just x -> liftIO $ putStrLn x
               Nothing -> return ()
              loop symCount
 
@@ -29,3 +39,26 @@ re n s = case runExcept (runStateT (readExpr s) n) of
           Left err -> (Just $ errString err, n)
           Right (Just x, n') -> (Just $ prStr x, n')
           Right (Nothing, n') -> (Nothing, n')
+
+readHistory :: IO (Seq String)
+readHistory = do hdir <- getAppDataDirectory "swearjure"
+                 createTree hdir
+                 let file = hdir </> fromText "history"
+                 isF <- isFile file
+                 if isF
+                    then do ls <- lines <$> readFile (encodeString file)
+                            mapM_ addHistory ls
+                            return $ fromList ls
+                    else return S.empty
+
+writeHistory :: Seq String -> IO ()
+writeHistory hist = do hdir <- getAppDataDirectory "swearjure"
+                       createTree hdir
+                       let fname = encodeString (hdir </> fromText "history")
+                       let hist' = takeLast 1000 hist
+                       writeFile fname (unlines $ toList hist')
+
+-- | returns the last n elements in the seq as a seq, or the seq itself if it
+-- contains less than n elements.
+takeLast :: Int -> Seq a -> Seq a
+takeLast n s = S.drop (S.length s - n) s
